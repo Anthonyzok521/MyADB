4e2e72bb6b349c228bf9d811115cb6a3c1c859b9
Anthonyzok521 <anthonyzok521@gmail.com>
2022-03-20 18:17:33 -0400

    Se implementó el colorama

--- MyADB.py
+++ MyADB.py
@@ -1,18 +1,19 @@
 import os
-telefono = str(os.system('adb.exe device'))
-os.system('clear')
-print('''\033[31m
-         _     _  _  _ \033[32m    _________________   _______
-        \033[31m| \   / |\ \/ /\033[32m   |  ___  ||   __ \ \ |  _  \ \ 
-        \033[31m|  \_/  | \  /\033[32m    | |   | ||  |  | | ||_|_|_/ /
-        \033[31m| |\_/| | / / \033[32m    | |___| ||  |__| | ||  _  \ \ 
-        \033[31m|_|   |_|/_/  \033[32m    |_|   |_||______/_/ |_|_|_/_/
+from colorama import init, Fore, Back, Style
+init()
+os.system('cls')
+R = Fore.RED
+V = Fore.GREEN
+B = Fore.WHITE
+print(R+'''
+         _     _  _  _ '''+V+'''    _________________   _______
+        '''+R+'''| \   / |\ \/ /'''+V+'''   |  ___  ||   __ \ \ |  _  \ \ 
+        '''+R+'''|  \_/  | \  /'''+V+'''    | |   | ||  |  | | ||_|_|_/ /
+        '''+R+'''| |\_/| | / / '''+V+'''    | |___| ||  |__| | ||  _  \ \ 
+        '''+R+'''|_|   |_|/_/  '''+V+'''    |_|   |_||______/_/ |_|_|_/_/
 ''')
-
-print(telefono)
-print('Coloque el nombre del archivo que desea pasar a la ruta de su : ')
-print('\033[0m')
-nombre = input()
+print('Coloque el nombre del archivo que desea pasar a la ruta de su telefono: '+B)
+nombre = input('> ')
 os.system('adb.exe push \"'+nombre+'\" /sdcard/')
 print('Listo')
 
