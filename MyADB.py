8c55533239dbd443e0056e3744f7833e81f505f8
Anthony Carrillo <anthonyzok521@gmail.com>
2022-03-20 15:38:29 -0400

    Script Python, solo tiene el comando adb push para usar.

---
+++ MyADB.py
@@ -0,0 +1,18 @@
+import os
+telefono = str(os.system('adb.exe devices -l'))
+os.system('clear')
+print('''\033[31m
+         _     _  _  _ \033[32m    _________________   _______
+        \033[31m| \   / |\ \/ /\033[32m   |  ___  ||   __ \ \ |  _  \ \ 
+        \033[31m|  \_/  | \  /\033[32m    | |   | ||  |  | | ||_|_|_/ /
+        \033[31m| |\_/| | / / \033[32m    | |___| ||  |__| | ||  _  \ \ 
+        \033[31m|_|   |_|/_/  \033[32m    |_|   |_||______/_/ |_|_|_/_/
+''')
+
+
+print('Coloque el nombre del archivo que desea pasar a la ruta de su '+telefono+': ')
+print('\033[0m')
+nombre = input()
+os.system('adb.exe push \"'+nombre+'\" /sdcard/')
+print('Listo')
+
